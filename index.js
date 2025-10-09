b5587bfe754e2640b310d730fbdd7b61588ad22a
123099663 <123099663@qq.com>
2025-10-09 13:10:54 +0800

    init

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ sign().then(res => {
     // if (res.resCode === '1112') {
     //     console.log('今日已签到，无需重复签到');
     // } else {
-    pushWx('签到结果', JSON.stringify(res));
+    pushWx(res.resDesc || '签到结果', JSON.stringify(res));
     // }
 }).catch(err => {
     console.error('签到过程出现错误:', err);
