29360b8c2a46461debb1a4f3a80561ef8a06305e
123099663 <123099663@qq.com>
2025-10-09 12:04:20 +0800

    init

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-fetch({
+fetch('http://dlyd.tuopukeji.cn/dlyd/api/attend/sign?openid=oN5WLjonhiTq-o_OiPF1Zpp95C3E&f=member', {
     method: 'POST',
-    url: 'http://dlyd.tuopukeji.cn/dlyd/api/attend/sign?openid=oN5WLjonhiTq-o_OiPF1Zpp95C3E&f=member',
 }).then(res => res.json())
     .then(res => {
         console.log(res);
