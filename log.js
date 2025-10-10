8df4ea94cec90429ffa6081d2d4b38c04a036e66
123099663 <123099663@qq.com>
2025-10-10 16:21:19 +0800

    revert time zone

--- log.js
+++ log.js
@@ -2,7 +2,11 @@ const originalConsole = { ...console };
 
 function getTimestamp() {
     const date = new Date();
-    return `[${date.toISOString().replace('T', ' ').replace('Z', '')}] `;
+    // Convert to Beijing time (UTC+8)
+    const beijingOffset = 8 * 60; // minutes
+    const localOffset = date.getTimezoneOffset(); // minutes
+    const beijingTime = new Date(date.getTime() + (beijingOffset + localOffset) * 60000);
+    return `[${beijingTime.toISOString().replace('T', ' ').replace('Z', '')}] `;
 }
 
 Object.keys(originalConsole).forEach((method) => {
