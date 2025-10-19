b8bad2f472f72265da2a02c6956be130f56fd6cc
123099663 <123099663@qq.com>
2025-10-19 12:27:00 +0800

    update contact

--- context.js
+++ context.js
@@ -1,8 +1,8 @@
 const context = {
     WX_PUSHSERVICE_URL: process.env.WX_PUSHSERVICE_URL,  // e.g., https://sctapi.ftqq.com/${sckey}.send?text=${title}&desp=${desp}
-    WX_PUSHSERVICE_KEY: process.env.WX_PUSHSERVICE_KEY, // get from https://sct.ftqq.com/
+    WX_PUSHSERVICE_KEY: process.env.WX_PUSHSERVICE_KEY, // get from https://sct.ftqq.com/r/18677
     DLYD_SIGN_URL: process.env.DLYD_SIGN_URL, // e.g., http://dlyd.tuopukeji.cn/dlyd/api/attend/sign?openid=${openid}&f=member
-    DLYD_OPENID: process.env.DLYD_OPENID, // e.g., contact the developer to get your openid, WX: Quimby001
+    DLYD_OPENID: process.env.DLYD_OPENID, // e.g., contact the developer to get your openid, QQ: 123099663
 };
 
 exports = module.exports = context;
